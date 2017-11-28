4fcbf7ff0c22b0679ca14b5794e3bec50f78c01a
yasminhillis <jasminehillis94@gmail.com>
2017-11-28 13:55:09 +0200

    add homeAnd front handlers

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const router = require('./router.js');
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 4000;
 
-const server = http.createServer(router.router);
+const server = http.createServer(router);
 server.listen(port, (error) =>{
   console.log('Server running on port: ' + port);
 });
