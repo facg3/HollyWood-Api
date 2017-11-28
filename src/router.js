66f8239612b1969ca2d353c0719c8e24035e2c8d
ismail2009 <somaaa2009@live.com>
2017-11-28 14:23:55 +0200

    getting everything right

--- src/router.js
+++ src/router.js
@@ -3,14 +3,16 @@ const handlers = require('./handler.js');
 
 const router = (request, response) => {
   const endpoint = request.url;
-  if (request.url === "/") {
+  if (endpoint === "/") {
     handlers.homePageHandler(request, response);
   } else if (request.url.startsWith("/front")) {
     handlers.frontHandler(request, response);
-  } else if (true) {
+  } else if (endpoint === '/search') {
     // Sum3a and Sultan
+    handlers.searchHandler(request , response);
   } else {
-
+    response.writeHead(404,"Content-Type: text/html");
+    response.end("<h1>No Files Found</h1>");
   }
 }
 
