ac161d76b5ebb0210b4c72c9cd9a3f7d2e124c14
Sultan H <mrsultanassi@gmail.com>
2017-11-28 16:29:05 +0200

    finishind search handler. #4

--- front/js/index.js
+++ front/js/index.js
@@ -1,11 +1,12 @@
 function fetch(url, searchValue, callback){
   var xhr = new XMLHttpRequest();
-  xhr.onreadychange = function () {
-    if (xhr.readystate == 4){
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4){
       if (xhr.status == 200) {
-        callback(null, JSON.parse(xhr.responseText));
+        console.log(JSON.parse(xhr.responseText));
+        callback(JSON.parse(xhr.responseText));
       } else {
-        alert('Error Occured!!, on: "+url+" with response: "+error);
+        alert("Error Occured!!, on: "+url+" with response: "+error);
       }
     }
   }
